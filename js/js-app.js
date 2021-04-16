4a1e2a2ce4dd5bf58825db0a23c14f4790fbed13
Hossam <hossamibrahim66666@gmail.com>
2021-04-16 21:52:39 +0200

    real-Estate-project

--- js/js-app.js
+++ js/js-app.js
@@ -1,3 +1,5 @@
+/* Global variables */ 
+
 let myNave=document.getElementById('nav-head'),
 myLogo=document.getElementById('logo'),
 myLinks=document.querySelectorAll('.ul-head li a'),
@@ -8,82 +10,59 @@ my_moreImage=document.querySelectorAll('.more'),
 my_moreImage_Array=Array.from(my_moreImage),
 
 myLink=document.querySelectorAll('.nav-toggle .container-span span')
-console.log(myLink[1].children)
+
 
 myContain_span=document.querySelectorAll('.container-span span')
 mycontain_Li=document.querySelectorAll('.ul-head li')
 
+for(var i=0; i<4;  i++){
 
+    let firstLinkli=document.createElement('a')
+    let firstLink=document.createElement('a')
 
-/* start dynamic links  */
-let firstLinkli=document.createElement('a')
-firstLinkli.setAttribute('href','#')
-firstLinkli.setAttribute('id','firstLinkli_id')
-firstLinkli.textContent="Home"
-firstLinkli.onclick=function(){
-    removeAll()
-    firstLinkli.classList.add('heighlite')
+   
+    mycontain_Li[1].appendChild(firstLinkli);
+//----------------------------------------
+   
+    myContain_span[i].appendChild(firstLink);
+    
+    if(i==0){
+    firstLinkli.setAttribute('href','#0')
+    firstLinkli.textContent='Home'
+    mycontain_Li[i].appendChild(firstLinkli);
+    myContain_span[i].appendChild(firstLink);
+    firstLink.textContent='Home'
+    firstLink.setAttribute('href','#0')
+    }
+    if(i==1){
+        firstLinkli.setAttribute('href','#1')
+        firstLinkli.textContent='servecis'
+    mycontain_Li[i].appendChild(firstLinkli);
+    myContain_span[i].appendChild(firstLink);
+    firstLink.textContent='servecis'
+    firstLink.setAttribute('href','#1')
+    }
+    if(i==2){
+        firstLinkli.setAttribute('href','#2')
+    firstLinkli.textContent='Conuct us'
+    mycontain_Li[i].appendChild(firstLinkli);
+    myContain_span[i].appendChild(firstLink);
+    firstLink.textContent='Conuct us'
+    firstLink.setAttribute('href','#2')
+
+    }
+    if(i==3){
+        firstLinkli.setAttribute('href','#3')
+        firstLinkli.textContent='news'
+    mycontain_Li[i].appendChild(firstLinkli);
+    myContain_span[i].appendChild(firstLink);
+    firstLink.textContent='news'
+    firstLink.setAttribute('href','#3')
+    }
+    
 }
 
-let scondeLinkli=document.createElement('a')
-scondeLinkli.setAttribute('href','#our-servecis')
-scondeLinkli.setAttribute('id','scondeLinkli_id')
-scondeLinkli.textContent="servecis"
-scondeLinkli.onclick=function(){
-    removeAll()
-    scondeLinkli.classList.add('heighlite')
-}
 
-let thirdlinkli=document.createElement('a')
-thirdlinkli.setAttribute('href','#connecting')
-thirdlinkli.setAttribute('id','thirdlinkli_id')
-thirdlinkli.textContent="Conuct us"
-thirdlinkli.onclick=function(){
-    removeAll()
-    thirdlinkli.classList.add('heighlite')
-}
-
-let fourthLinkli=document.createElement('a')
-fourthLinkli.setAttribute('href','#ournews')
-fourthLinkli.setAttribute('id','fourthLinkli_id')
-fourthLinkli.textContent="news"
-fourthLinkli.onclick=function(){
-    removeAll()
-    fourthLinkli.classList.add('heighlite')
-}
-
-/* End dynamic links  */
-
-/* start dynamic links 2  */
-let firstLink=document.createElement('a')
-firstLink.setAttribute('href','#')
-firstLink.textContent="Home"
-
-let scondeLink=document.createElement('a')
-scondeLink.setAttribute('href','#our-servecis')
-scondeLink.textContent="servecis"
-
-let thirdlink=document.createElement('a')
-thirdlink.setAttribute('href','#connecting')
-thirdlink.textContent="Conuct us"
-
-let fourthLink=document.createElement('a')
-fourthLink.setAttribute('href','#ournews')
-fourthLink.textContent="news"
-/* end dynamic links 2  */
-
-
-/*appendchild dynamic links */
-mycontain_Li[0].appendChild(firstLinkli); 
-mycontain_Li[1].appendChild(scondeLinkli); 
-mycontain_Li[2].appendChild(thirdlinkli); 
-mycontain_Li[3].appendChild(fourthLinkli); 
-
-myContain_span[0].appendChild(firstLink); 
-myContain_span[1].appendChild(scondeLink); 
-myContain_span[2].appendChild(thirdlink); 
-myContain_span[3].appendChild(fourthLink); 
-/*appendchild dynamic links */
 
 
 my_button.addEventListener('click',function(){
