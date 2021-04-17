79de5162c4314669bd89d2bad3faf0e8b241d74b
Hossam <hossamibrahim66666@gmail.com>
2021-04-17 22:04:39 +0200

    real-Estate-project

--- js/js-app.js
+++ js/js-app.js
@@ -1,58 +1,75 @@
-/* Global variables */ 
+/* start Global variables */ 
 
 let myNave=document.getElementById('nav-head'),
-myLogo=document.getElementById('logo'),
-myLinks=document.querySelectorAll('.ul-head li a'),
-myLinks_Array=Array.from(myLinks),
 myNave_toggle=document.getElementById('nave-toggle'),
 my_button=document.querySelector('.more-detail-bt')
 my_moreImage=document.querySelectorAll('.more'),
 my_moreImage_Array=Array.from(my_moreImage),
-
 myLink=document.querySelectorAll('.nav-toggle .container-span span')
-
-
 myContain_span=document.querySelector('.container-span')
 mycontain_Li=document.querySelector('.ul-head')
 
+/* end Global variables */ 
 
-for(var i=0; i<4; i++){
+
+for(let i=0; i<4; i++){
     let li_item=document.createElement('li')
     let link_item=document.createElement('a')
     link_item.setAttribute('id','link'+i)
+    link_item.setAttribute('data-link','section'+i)
+    link_item.setAttribute('class','links')
     li_item.appendChild(link_item)
     mycontain_Li.appendChild(li_item)
-
-    //--------------------------------------
-   
 }
 
+const all_links1=document.querySelectorAll('.links');
 
-for(var i=0; i<4; i++){
+// forEach for scroll
+all_links1.forEach(item=>{
+    item.addEventListener('click',()=>{
+        const el=document.getElementById(item.getAttribute('data-link'))
+
+        el.scrollIntoView({
+            behavior:"smooth",
+            block:"center"
+        })
+
+    })
+})
+
+for(let i=0; i<4; i++){
     let li_ite2=document.createElement('span')
     let link_ite2=document.createElement('a')
     link_ite2.setAttribute('id','lin'+i)
+    link_ite2.setAttribute('class','linkes')
+    link_ite2.setAttribute('data-link','section'+i)
     li_ite2.appendChild(link_ite2)
     myContain_span.appendChild(li_ite2)  
 }
+const all_links2=document.querySelectorAll('.linkes');
+
+all_links2.forEach(item2=>{
+    item2.addEventListener('click',()=>{
+        const el2=document.getElementById(item2.getAttribute('data-link'))
+
+        el2.scrollIntoView({
+            behavior:"smooth",
+            block:"center"
+        })
+
+    })
+})
+
 let mylinke_ho=document.getElementById('link0')
      mylinke_cona=document.getElementById('link1')
      mylinke_ser=document.getElementById('link2')
      mylinke_new=document.getElementById('link3')
 
      mylinke_new.textContent='new'
-     mylinke_ser.textContent='services'
-     mylinke_cona.textContent='connact us'
+     mylinke_ser.textContent='connact us'
+     mylinke_cona.textContent='services'
      mylinke_ho.textContent='Home'
 
-     mylinke_new.setAttribute('href','#3')
-     mylinke_ser.setAttribute('href','#1')
-     mylinke_cona.setAttribute('href','#2')
-     mylinke_ho.setAttribute('href','#0')
-
-
-
-//--------------------------------------------------------
 let mylinke_ho2=document.getElementById('lin0')
      mylinke_cona2=document.getElementById('lin1')
      mylinke_ser2=document.getElementById('lin2')
@@ -62,11 +79,6 @@ let mylinke_ho2=document.getElementById('lin0')
      mylinke_ser2.textContent='services'
      mylinke_cona2.textContent='connact us'
      mylinke_ho2.textContent='Home'
-
-     mylinke_new2.setAttribute('href','#3')
-     mylinke_ser2.setAttribute('href','#1')
-     mylinke_cona2.setAttribute('href','#2')
-     mylinke_ho2.setAttribute('href','#0')
 
 
 my_button.addEventListener('click',function(){
@@ -115,70 +127,54 @@ myClose_icon.onclick=function(){
     
 }
 
+//--highlite of  icon
 
-/*--------------------------------------------------------------------------------------- */
-
-/*function of to add active  */  
-mylinke_ho.onclick=function(){
-
-removeAll()
-mylinke_ho.classList.add('heighlite')
+all_links1[0].onclick=function(){
+    removeall()
+    all_links1[0].classList.add('heighlite')
 }
-mylinke_ser.onclick=function(){
-    
-    removeAll()
-    mylinke_ser.classList.add('heighlite')
+all_links1[1].onclick=function(){
+    removeall()
+    all_links1[1].classList.add('heighlite')
 }
-mylinke_cona.onclick=function(){
-    removeAll()
-    mylinke_cona.classList.add('heighlite')
-
+all_links1[2].onclick=function(){
+    removeall()
+    all_links1[2].classList.add('heighlite')
 }
-mylinke_new.onclick=function(){
-    removeAll()
-    mylinke_new.classList.add('heighlite')
-
-}
-
-/*function of to remove active  */  
-function removeAll(){
-    mylinke_ho.classList.remove('heighlite')
-    mylinke_ser.classList.remove('heighlite')
-    mylinke_cona.classList.remove('heighlite')
-    mylinke_new.classList.remove('heighlite')
-}
-/*function of to remove active in toggle_icon  */  
-function removeAll2(){
-    mylinke_ho2.classList.remove('heighlite')
-    mylinke_ser2.classList.remove('heighlite')
-    mylinke_cona2.classList.remove('heighlite')
-    mylinke_new2.classList.remove('heighlite')
+all_links1[3].onclick=function(){
+    removeall()
+    all_links1[3].classList.add('heighlite')
 }
 
 
-//-----------------------------------------------------------------------------
+   function removeall() { all_links1.forEach(el=>{
+        el.classList.remove('heighlite')
+    })
+}
 
-/*function of to add active in toggle icon  */  
-mylinke_ho2.onclick=function(){
- 
-    removeAll()
-    mylinke_ho2.classList.add('heighlite')
-    }
-    mylinke_ser2.onclick=function(){
-        
-        removeAll()
-        mylinke_ser2.classList.add('heighlite')
-    }
-    mylinke_cona2.onclick=function(){
-        removeAll()
-        mylinke_cona2.classList.add('heighlite')
+//--highlite of toggle icon
+
+all_links2[0].onclick=function(){
+    removeall2()
+    all_links2[0].classList.add('heighlite')
+}
+all_links2[1].onclick=function(){
+    removeall2()
+    all_links2[1].classList.add('heighlite')
+}
+all_links2[2].onclick=function(){
+    removeall2()
+    all_links2[2].classList.add('heighlite')
+}
+all_links2[3].onclick=function(){
+    removeall2()
+    all_links2[3].classList.add('heighlite')
+}
+
+
+   function removeall2() { all_links2.forEach(el=>{
+        el.classList.remove('heighlite')
+    })
+}
   
-    }
-    mylinke_new2.onclick=function(){
-        removeAll()
-        mylinke_new2.classList.add('heighlite')
- 
-    }
     
-
-   
